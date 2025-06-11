3ec8405bedbf1b0e4c1f52d887f05e13f6f8533f
Matthias Hryniszak <padcom@gmail.com>
2025-06-11 22:12:33 +0200

    Added option to run eiher with chat completions or responses api

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- <pre>{{ JSON.stringify(messages, null, 2) }}</pre> -->
   <div class="messages-container">
     <div v-for="message in messages" :key="message.id" class="markdown-body message" :class="{ [message.role]: true }"
       v-html="marked(message.content, { renderer })"
@@ -20,6 +21,7 @@ import { OpenAI } from 'openai'
 import type { ChatCompletionMessage, ChatCompletionMessageParam } from 'openai/resources/index.mjs'
 import { marked } from 'marked'
 import type { Stream } from 'openai/core/streaming.mjs'
+import type { ResponseIncompleteEvent, ResponseInputItem } from 'openai/resources/responses/responses.mjs'
 
 const renderer = new marked.Renderer()
 renderer.link = function({ href, title, text }) {
@@ -31,12 +33,15 @@ interface Message {
   name?: string
   role: string
   content: string
+  reasoning?: boolean
 }
 
 const messages = ref<Message[]>([])
 const question = ref('what is the highest mountain?')
-const stream = shallowRef<Stream<OpenAI.Chat.Completions.ChatCompletionChunk>>()
-const processing = computed(() => Boolean(stream.value))
+const streamResponses = shallowRef<Stream<OpenAI.Responses.ResponseStreamEvent>>()
+const streamChat = shallowRef<Stream<OpenAI.Chat.Completions.ChatCompletionChunk>>()
+const processing = computed(() => Boolean(streamResponses.value) || Boolean(streamChat.value))
+const type = ref<'responses' | 'chat'>('responses')
 
 const api = new OpenAI({
   baseURL: import.meta.env.VITE_APP_OPENAI_BASE_URL,
@@ -44,47 +49,91 @@ const api = new OpenAI({
   dangerouslyAllowBrowser: true,
 })
 
-async function ask() {
+const previousResponseId = ref('')
+
+async function askResponses() {
+  console.log('asking responses!')
+
   if (!question.value) return
 
   messages.value.unshift({ role: 'user', content: question.value })
   question.value = ''
 
-  stream.value = await api.chat.completions.create({
-    model: 'gemma3:4b',
-    messages: [...messages.value as Array<ChatCompletionMessage>].reverse(),
+  streamResponses.value = await api.responses.create({
+    model: import.meta.env.VITE_APP_MODEL,
+    input: messages.value[0].content,
+    tools: [{ type: 'web_search_preview' }],
+    previous_response_id: previousResponseId.value,
+    store: true,
+    stream: true,
+    reasoning: { effort: 'high' },
+  })
+
+  messages.value.unshift({ role: 'assistant', content: '' })
+
+  try {
+    for await (const event of streamResponses.value) {
+      if (event.type === 'response.reasoning.delta') {
+        messages.value[0].reasoning = true
+        messages.value[0].content += event.delta
+      } else if (event.type === 'response.reasoning.done') {
+        messages.value.unshift({ role: 'assistant', content: '' })
+      } else if (event.type === 'response.output_text.delta') {
+        messages.value[0].content += event.delta
+      } else if (event.type === 'response.created') {
+        previousResponseId.value = event.response.id
+      } else {
+        console.log(event.type, event)
+      }
+    }
+  } finally {
+    streamResponses.value = undefined
+  }
+}
+
+async function askChat() {
+  console.log('asking chat!')
+
+  if (!question.value) return
+
+  messages.value.unshift({ role: 'user', content: question.value })
+  question.value = ''
+
+  streamChat.value = await api.chat.completions.create({
+    model: import.meta.env.VITE_APP_MODEL,
+    messages: [...messages.value as ChatCompletionMessageParam[]].reverse(),
     stream: true,
   })
 
   messages.value.unshift({ role: 'assistant', content: '' })
 
   try {
-    for await (const event of stream.value) {
-      if (messages.value[0].role !== event.choices[0].delta.role) {
-        if (!messages.value[0].role && event.choices[0].delta.role) {
-          messages.value[0].role = event.choices[0].delta.role
-        } else if (event.choices[0].delta.role) {
-          messages.value.unshift({ role: event.choices[0].delta.role, content: '' })
-        }
-      }
-
-      if (event.choices[0].delta.content) {
-        messages.value[0].content += event.choices[0].delta.content
-      }
+    for await (const event of streamChat.value) {
+      messages.value[0].content += event.choices[0].delta.content
     }
   } finally {
-    stream.value = undefined
+    streamChat.value = undefined
+  }
+}
+
+function ask() {
+  switch (type.value) {
+    case 'responses': return askResponses()
+    case 'chat': return askChat()
   }
 }
 
 function abort() {
-  stream.value?.controller.abort()
+  streamResponses.value?.controller.abort()
+  streamChat.value?.controller.abort()
 }
 
 function newChat() {
   messages.value = [
     { role: 'system', content: 'You are a helpful assistant. You come from [LM Studio](https://lmstudio.ai)' },
   ]
+
+  previousResponseId.value = ''
 }
 
 onMounted(newChat)
