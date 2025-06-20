403f926f5c22c27e23aa525adb8b6d297db068be
Matthias Hryniszak <padcom@gmail.com>
2025-06-21 01:55:13 +0200

    Replaced manual addition of message to the list with `addMessage()`

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,8 @@ import type { Stream } from 'openai/core/streaming.mjs'
 
 import { uuid, Chat, Messages, Message, Prompt, addMessage, type ChatMessage } from '@padcom/chat-ui'
 import "@padcom/chat-ui/dist/index.css"
-import "@padcom/chat-ui-formatter-marked"
+// import "@padcom/chat-ui-formatter-marked"
+import "@padcom/chat-ui-formatter-json"
 import 'github-markdown-css'
 
 interface ToolCall {
@@ -62,7 +63,7 @@ async function askResponses(question: string) {
   console.log('asking responses!')
 
   processing.value = true
-  messages.value.push({ id: uuid(), role: 'user', content: question })
+  addMessage(messages, { role: 'user', content: question })
 
   try {
     streamResponses.value = await api.responses.create({
@@ -75,14 +76,12 @@ async function askResponses(question: string) {
       reasoning: { effort: 'high' },
     })
 
-    addMessage(messages, { id: uuid(), role: 'assistant', content: '' })
+    addMessage(messages, { role: 'assistant', content: '' })
   } catch (e) {
     processing.value = false
-    addMessage(messages, { id: uuid(), role: 'error', content: `${e}` })
+    addMessage(messages, { role: 'error', content: `${e}` })
     throw e
   }
-
-  console.log(3)
 
   try {
     for await (const event of streamResponses.value!) {
@@ -90,7 +89,7 @@ async function askResponses(question: string) {
         messages.value.at(-1)!.reasoning = true
         messages.value.at(-1)!.content! += event.delta
       } else if (event.type === 'response.reasoning.done') {
-        messages.value.push({ id: event.item_id, role: 'assistant', content: '' })
+        addMessage(messages, { id: event.item_id, role: 'assistant', content: '' })
       } else if (event.type === 'response.output_text.delta') {
         messages.value.at(-1)!.content += event.delta
       } else if (event.type === 'response.created') {
@@ -123,7 +122,7 @@ const getFruitPrice: ChatCompletionTool = {
 }
 
 async function processChatMessages() {
-  messages.value.push({ id: uuid(), role: 'assistant', content: '' })
+  addMessage(messages, { role: 'assistant', content: '' })
 
   function processTextMessage(choice: OpenAI.Chat.Completions.ChatCompletionChunk.Choice) {
     if (choice.delta.content === '<think>') {
@@ -131,7 +130,7 @@ async function processChatMessages() {
       messages.value.at(-1)!.reasoning = true
     } else if (choice.delta.content === '</think>' && messages.value.at(-1)!.reasoning) {
       messages.value.at(-1)!.content += choice.delta.content
-      // messages.value.push({ role: 'assistant', content: '' })
+      // addMessage(messages, { role: 'assistant', content: '' })
     } else if (choice.delta.content) {
       messages.value.at(-1)!.content += choice.delta.content
     }
@@ -191,17 +190,17 @@ async function processChatMessages() {
 async function askChat(question: string) {
   console.log('asking chat!')
 
-  messages.value.push({ id: uuid(), role: 'user', content: question })
+  addMessage(messages, { role: 'user', content: question })
 
   try {
     streamChat.value = await api.chat.completions.create({
       model: import.meta.env.VITE_APP_MODEL,
-      messages: [...messages.value as ChatCompletionMessageParam[]].reverse(),
+      messages: messages.value as ChatCompletionMessageParam[],
       stream: true,
-      tools: [getFruitPrice],
+      // tools: [getFruitPrice],
     })
   } catch (e) {
-    addMessage(messages, { id: uuid(), role: 'error', content: `${e}` })
+    addMessage(messages, { role: 'error', content: `${e}` })
     return
   }
 
@@ -210,7 +209,7 @@ async function askChat(question: string) {
   if (messages.value.at(-1)!.tool_calls) {
     for (const toolCall of messages.value.at(-1)!.tool_calls!) {
       console.log('Requested a call to', toolCall.type, JSON.stringify(toolCall[toolCall.type]))
-      messages.value.push({ id: uuid(), role: 'tool', content: '$5', tool_call_id: toolCall.id })
+      addMessage(messages, { role: 'tool', content: '$5', tool_call_id: toolCall.id })
     }
 
     streamChat.value = await api.chat.completions.create({
@@ -238,7 +237,7 @@ function abort() {
 function newChat() {
   abort()
   messages.value = [
-    { id: uuid(), role: 'system', content: 'You are a helpful assistant. You come from [LM Studio](https://lmstudio.ai)' },
+    { role: 'system', content: 'You are a helpful assistant. You come from [LM Studio](https://lmstudio.ai)' },
   ]
 
   previousResponseId.value = ''
